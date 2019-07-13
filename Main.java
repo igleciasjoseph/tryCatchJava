35e8c41e5f6e7b22cde1825b94975d2b313654c8
Joseph Iglecias <igleciasjoseph@gmail.com>
2019-07-13 19:39:36 -0400

    cant figure it out so im gonna try later

---
+++ Main.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        ArrayList<Object> myList = new ArrayList<Object>();
+        myList.add("13");
+        myList.add("hello world");
+        myList.add(48);
+        myList.add("Goodbye World");
+        for (int i = 0; i < myList.size(); i++) {
+            try {
+                Integer castedValue = (Integer) myList.get(i);
+                System.out.println(castedValue);
+            } catch (Exception e) {
+                if (castedValue == "") {
+                    System.out.println("Sorry that is not a number");
+                }
+            }
+        }
+    }
+}
